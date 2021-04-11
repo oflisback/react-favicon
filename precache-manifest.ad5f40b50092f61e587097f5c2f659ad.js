e226908bd9f263f98eed90552a126b8054180150
Ola Flisbäck <ola.flisback@gmail.com>
2021-04-11 10:11:00 +0200

    Updates

---
+++ precache-manifest.ad5f40b50092f61e587097f5c2f659ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d669e387d911a284db599ab2e5eb941",
+    "url": "./index.html"
+  },
+  {
+    "revision": "949f001209d17c16b44e",
+    "url": "./static/js/2.344ec07d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.344ec07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f180c972e813fe854f",
+    "url": "./static/js/main.d9bac3da.chunk.js"
+  },
+  {
+    "revision": "31583b7914db86751055",
+    "url": "./static/js/runtime-main.cac0dfa0.js"
+  }
+]);
