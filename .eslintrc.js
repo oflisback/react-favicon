61bd52e563491f001f6a332b06d11dff92fd6944
Ola Flisbäck <ola.flisback@gmail.com>
2023-10-21 20:49:19 +0200

    chore: clean up eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
+  ],
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
 }
