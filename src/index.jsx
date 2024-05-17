0551f3b03d3075bc702280639b927bce01d89382
Magnus Claesson <magnus.claesson@gmail.com>
2024-05-17 08:42:39 +0200

    fix: Removed defaultProps from component, resolves #54

--- src/index.jsx
+++ src/index.jsx
@@ -114,15 +114,15 @@ const updateHtmlIconLink = (keepIconLink) => {
 }
 
 const Favicon = ({
-  iconSize,
-  alertCount,
-  alertFillColor,
-  alertTextColor,
-  animated,
-  animationDelay,
-  keepIconLink,
-  renderOverlay,
-  url,
+  iconSize = DefaultCanvasSize,
+  alertCount = null,
+  alertFillColor = 'red',
+  alertTextColor = 'white',
+  animated = true,
+  animationDelay = 500,
+  keepIconLink = () => false,
+  renderOverlay = null,
+  url = null,
 }) => {
   const animationIndex = useRef(0)
   const animationTickIntervalId = useRef(null)
@@ -181,18 +181,6 @@ const Favicon = ({
   }
 
   return null
-}
-
-Favicon.defaultProps = {
-  iconSize: DefaultCanvasSize,
-  alertCount: null,
-  alertFillColor: 'red',
-  alertTextColor: 'white',
-  animated: true,
-  animationDelay: 500,
-  keepIconLink: () => false,
-  renderOverlay: null,
-  url: null,
 }
 
 Favicon.propTypes = {
